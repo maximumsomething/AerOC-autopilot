220bc263f1030bded4bf92f48df304606e59a439
theoHC <theo_coulson@me.com>
2023-01-16 11:14:58 -0500

    whoops, that didn't compile!

--- relay-control/relay-control.ino
+++ relay-control/relay-control.ino
@@ -3,25 +3,25 @@
 //pins D6-10: servo output
 
 void setup() {
-  pinMode(0, INPUT);
-  pinMode(1, INPUT);
   pinMode(2, INPUT);
   pinMode(3, INPUT);
   pinMode(4, INPUT);
   pinMode(5, INPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
   pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
-
-  int sourceDutyCycle = 0;
-  boolean onAutopilot = false;
-  boolean checkedStatusThisCycle = false;
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
+int sourceDutyCycle = 0;
+boolean onAutopilot = false;
+boolean checkedStatusThisCycle = false;
+
 void loop() {
-  if(digitalRead(6) == HIGH && sourceDutyCycle = 0){
+  if(digitalRead(6) == HIGH && sourceDutyCycle == 0){
     sourceDutyCycle = micros();
     checkedStatusThisCycle = false;
   }else if(digitalRead(5) == HIGH && micros() - sourceDutyCycle >= 1900){
@@ -35,10 +35,9 @@ void loop() {
   }
 
   if(onAutopilot){
-    digitalWrite();
   }else{
     for(int i = 0; i++; i<6){
-      digitalWrite(6 + i, digitalRead(i));
+      digitalWrite(8 + i, digitalRead(2+ i));
     }
   }
 }
