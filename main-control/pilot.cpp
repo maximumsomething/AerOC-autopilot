d2d47ee7b2cc2765ed50e4e0b9c9ce6e769e08df
theoHC <theo_coulson@me.com>
2023-01-16 17:34:46 -0500

    added kpid class

--- main-control/pilot.cpp
+++ main-control/pilot.cpp
@@ -29,3 +29,39 @@ void pilotloop() {
 
 	// telemetry all control outputs and intermediate crap
 }
+
+class kpid {
+	public:
+	explicit kpid(float Kc, float Kp, float Ki, float Kd) : Kc(Kc), Kp(Kp), Ki(Ki), Kd(Kd){}
+
+	float update(float newTarget, float input){
+		target = newTarget;
+		return update(input);
+	}
+	float update(float input){
+		unsigned long curTime = micros();
+		unsigned long dt = curTime - timeLastCalled;
+		error = input - target; // calculate current error
+
+		if(Ki != 0){
+			errInt += error * (dt / 1000000.0);
+		}
+		if(Kd != 0){
+			errDeriv = (error - prevError)/(dt / 1000000.0);
+		}
+
+		timeLastCalled = curTime;
+		prevError = error;
+
+		return Kc * target + Kp * error + Ki * errInt + Kd * errDeriv;
+	}
+
+	private:
+	float Kc, Kp, Ki, Kd;
+	float target;
+	float error, prevError;
+	float input;
+	float errInt;
+	float errDeriv;
+	unsigned long timeLastCalled;
+};
