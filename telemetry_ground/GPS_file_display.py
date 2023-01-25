1306086ae75f80fc201bed9a65a1434dc61973d9
theoHC <theo_coulson@me.com>
2023-01-25 16:46:05 -0500

    added timestamps to the routepoints

--- telemetry_ground/GPS_file_display.py
+++ telemetry_ground/GPS_file_display.py
@@ -4,13 +4,13 @@
 import sys
 import Telem_parser as tp
 
-print("Type, Latitude, Longitude")
+print("Type, Time, Latitude, Longitude")
 
 desiredLabel = "gpsFix"
 label = ""
 while(label := tp.read_line()):
     try:
         if(label == desiredLabel):
-            print("T,"+str(int(tp.get(desiredLabel, "lat"))*1.0e-7)+","+str(int(tp.get(desiredLabel, "lng"))*1.0e-7))
+            print("T,"+tp.values["GPSreckon"].timestamp+","+str(int(tp.get(desiredLabel, "lat"))*1.0e-7)+","+str(int(tp.get(desiredLabel, "lng"))*1.0e-7))
     except ValueError as e:
         pass
