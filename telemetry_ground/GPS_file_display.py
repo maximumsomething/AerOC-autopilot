dd7f433dfc4b9517a6a67c9b7c71d648ef61b87e
Max Mueggler <maximumsomething@gmail.com>
2023-01-30 15:10:04 -0500

    Output altitude in GPS log outputter

--- telemetry_ground/GPS_file_display.py
+++ telemetry_ground/GPS_file_display.py
@@ -4,13 +4,13 @@
 import sys
 import Telem_parser as tp
 
-print("Type, Time, Latitude, Longitude")
+print("Type, Time, Latitude, Longitude, Alt")
 
 desiredLabel = "gpsFix"
 label = ""
 while(label := tp.read_line()):
     try:
         if(label == desiredLabel):
-            print("T,"+tp.values[desiredLabel].timestamp+","+str(int(tp.get(desiredLabel, "lat"))*1.0e-7)+","+str(int(tp.get(desiredLabel, "lng"))*1.0e-7))
+            print("T,"+tp.values[desiredLabel].timestamp+","+str(int(tp.get(desiredLabel, "lat"))*1.0e-7)+","+str(int(tp.get(desiredLabel, "lng"))*1.0e-7) + "," + tp.get("pose", "altitude"))
     except ValueError as e:
         pass
